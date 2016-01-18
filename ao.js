fcf7fde29092807ad4dd12955deadebba7304f18
Adrian Oesch <adrianoesch@gmx.ch>
2016-01-18 16:04:55 +0100

    fix drag

--- ao.js
+++ ao.js
@@ -15,8 +15,7 @@ var Experiment = {
       };
     },
     dragover : function(ev) {
-      console.log(ev.target.id)
-      var classMatch = ev.originalEvent.dataTransfer.getData('class').slice(0,4)==ev.target.id.slice(0,4);
+      var classMatch = ev.dataTransfer.getData('class').slice(0,4)==ev.target.id.slice(0,4);
       if (classMatch && (ev.target.innerHTML=='' || ev.target.className=='menui')){
         ev.currentTarget.style.border = "1px dashed black";
         ev.preventDefault();
