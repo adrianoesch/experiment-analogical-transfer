cf0a6a3357af112b6f7d2f86f796db4dbef5cb1a
Adrian Oesch <adrianoesch@gmx.ch>
2016-01-18 18:55:01 +0100

    fix drag

--- ao.js
+++ ao.js
@@ -32,14 +32,14 @@ var Experiment = {
           t[i].addEventListener(evStr,evFunc);
         };
       };
-      addEvent("nameDivs","dragstart",Experiment.d.dragstart)
-      addEvent("relationDivs","dragstart",Experiment.d.dragstart)
-      addEvent("menui","dragover",Experiment.d.dragover)
-      addEvent("menui","drop",Experiment.d.drop)
-      addEvent("menui","dragleave",Experiment.d.dragleave)
-      addEvent("inputi","dragover",Experiment.d.dragover)
-      addEvent("inputi","drop",Experiment.d.drop)
-      addEvent("inputi","dragleave",Experiment.d.dragleave)
+      addEvent("nameDivs","dragstart",Experiment.d.dragstart);
+      addEvent("relationDivs","dragstart",Experiment.d.dragstart);
+      addEvent("menui","dragover",Experiment.d.dragover);
+      addEvent("menui","drop",Experiment.d.drop);
+      addEvent("menui","dragleave",Experiment.d.dragleave);
+      addEvent("inputi","dragover",Experiment.d.dragover);
+      addEvent("inputi","drop",Experiment.d.drop);
+      addEvent("inputi","dragleave",Experiment.d.dragleave);
     }
   },
   timeline : {
@@ -211,9 +211,9 @@ var Experiment = {
     },
     init : function(){
       var timeline = [];
-      // timeline.push(this.enter_fullscreen_block());
-      // timeline.push(this.consent_block());
-      // timeline.push(this.instructions_block());
+      timeline.push(this.enter_fullscreen_block());
+      timeline.push(this.consent_block());
+      timeline.push(this.instructions_block());
       timeline.push(this.hebb_block());
       timeline.push(this.demographics_block());
       timeline.push(this.exit_fullscreen_block());
