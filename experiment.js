dfdb04c29ee70b41eab662df8899822883d04509
Adrian Oesch <adrianoesch@gmx.ch>
2016-03-31 17:49:52 +0200

    adjust timeline

--- experiment.js
+++ experiment.js
@@ -422,14 +422,14 @@ var Experiment = {
     },
     init : function(){
       var timeline = [];
-      // timeline.push(this.enter_fullscreen_block());
-      // timeline.push(this.consent_block());
-      // timeline.push(this.instructions_block());
-      // timeline.push(this.hebb_block());
-      // timeline.push(this.exit_fullscreen_block());
-      // timeline.push(this.survey_block());
-      // timeline.push(this.rei_block());
-      // timeline.push(this.demographics_block());
+      timeline.push(this.enter_fullscreen_block());
+      timeline.push(this.consent_block());
+      timeline.push(this.instructions_block());
+      timeline.push(this.hebb_block());
+      timeline.push(this.exit_fullscreen_block());
+      timeline.push(this.survey_block());
+      timeline.push(this.rei_block());
+      timeline.push(this.demographics_block());
       timeline.push(this.confirmation_block());
       return timeline;
     }
